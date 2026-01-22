bc3d24588ad4e90a97ba693139941df1fc88e84e
VanshVala <102245923+Vanshvala23@users.noreply.github.com>
2026-01-22 10:16:42 +0530

    Updated

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,4 +1,10 @@
 import AboutUs from "../components/AboutUs";
+import Footer from "../components/Footer";
 export default function AboutPage() {
-    return <AboutUs />;
+    return (
+        <>
+    <AboutUs />
+        <Footer/>
+        </>
+    );
 }
